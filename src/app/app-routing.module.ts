ecf5d61ceb8c736634be53fcf2f190d7f0b98af4
drawde <edwarddiaz@gmail.com>
2019-12-27 12:05:19 -0500

    creando componentes de ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,34 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)},
   {
     path: 'saldo',
-    loadChildren: () => import('./billing/billing.module').then( m => m.BillingPageModule)
+    loadChildren: () => import('./pages/billing/billing.module').then( m => m.BillingPageModule)
   },
   {
     path: 'mensajes',
-    loadChildren: () => import('./messages/messages.module').then( m => m.MessagesPageModule)
+    loadChildren: () => import('./pages/messages/messages.module').then( m => m.MessagesPageModule)
   },
   {
     path: 'historial',
-    loadChildren: () => import('./invoices/invoices.module').then( m => m.InvoicesPageModule)
+    loadChildren: () => import('./pages/invoices/invoices.module').then( m => m.InvoicesPageModule)
+  },
+  {
+    path: 'componente/inicio',
+    loadChildren: () => import('./pages/components/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: 'componente/action-sheet',
+    loadChildren: () => import('./pages/components/action-sheet/action-sheet.module').then( m => m.ActionSheetPageModule)
+  },
+  {
+    path: 'componente/alert',
+    loadChildren: () => import('./pages/components/alert/alert.module').then( m => m.AlertPageModule)
+  },
+  {
+    path: 'componente/avatar',
+    loadChildren: () => import('./pages/components/avatar/avatar.module').then( m => m.AvatarPageModule)
   },
 ];
 
